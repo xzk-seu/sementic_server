5623a92880791237469c6d96fc59b1ff7d02fe12
cz <chenzhouwy@163.com>
2020-01-19 11:26:37 +0800

    fix account.py

--- sementic_server/source/ner_task/account.py
+++ sementic_server/source/ner_task/account.py
@@ -263,8 +263,8 @@ class Account:
         :return:
         """
         pattern_veh = re.compile(
-            "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(([0-9]{5}[DF])|(DF[0-9]{4})))|"
-            "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}"
+            "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(((-?)[0-9]{5}[DF])|((-?)DF[0-9]{4})))|"
+            "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(-?)[A-HJ-NP-Z0-9]{4}"
             "[A-HJ-NP-Z0-9挂学警港澳]{1})", re.IGNORECASE)
         vehicles = []
         for match in re.finditer(pattern_veh, raw_string):
@@ -328,7 +328,7 @@ class Account:
             is_veh = False
             if len(vehicles) != 0:
                 for veh in vehicles:
-                    if veh['begin'] <= begin <= veh['end']:
+                    if veh['begin'] <= begin <= veh['end'] and veh['end'] >= end:
                         is_veh = True
                     else:
                         account_list.clear()
